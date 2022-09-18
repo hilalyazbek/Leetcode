ddf194fcf9e49774a6fbb8d24210fe71256127f4
Hilal Yazbek <hilal_yazbeck@hotmail.com>
2022-09-19 00:20:09 +0300

    level 1, 2 problems

---
+++ CodingChallenges/Level1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+namespace CodingChallenges;
+
+public static class Level1
+{
+    public static int[] RunningSum(int[] nums)
+    {
+        int current = 0;
+        int[] result = nums.ToArray();
+        for (int i = 0; i < nums.Length; i++)
+        {
+            current = current + nums[i];
+            result[i] = current;
+        }
+
+        return result;
+    }
+
+    public static int PivotIndex(int[] nums)
+    {
+        if (nums == null || nums.Length == 0)
+        {
+            return -1;
+        }
+        int len = nums.Length;
+        int[] leftSum = new int[len];
+        int[] rightSum = new int[len];
+
+        leftSum[0] = nums[0];
+        rightSum[len - 1] = nums[len - 1];
+
+        for(int i=1; i < len; i++)
+        {
+            leftSum[i] = nums[i] + leftSum[i - 1];
+            rightSum[len - 1 - i] = nums[len - 1 - i] + rightSum[len - i];
+        }
+
+        for(int i = 0; i < len; i++)
+        {
+            if (leftSum[i] == rightSum[i])
+                return i;
+        }        
+        return -1;
+    }
+}
+
--- CodingChallenges/Program.cs
+++ CodingChallenges/Program.cs
@@ -1,3 +1,10 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World! initial commit");
+using CodingChallenges;
 
+Console.WriteLine("1480. Running Sum of 1d Array");
+Console.WriteLine("*****************************");
+Console.WriteLine(Level1.RunningSum(new int[] { 1, 5, 2, 6, 7, 4}));
+
+Console.WriteLine("724.Find Pivot Index");
+Console.WriteLine("********************");
+Console.WriteLine(Level1.PivotIndex(new int[] { 1, 7, 3, 6, 5, 6 }));
