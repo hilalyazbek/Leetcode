26f07cedea4c283967669a38be6adda434cfc8dd
Hilal Yazbek <hilal_yazbeck@hotmail.com>
2022-09-21 12:21:24 +0300

    is subsequence (attempt 1)

--- CodingChallenges/Level1.cs
+++ CodingChallenges/Level1.cs
@@ -29,18 +29,71 @@ public static class Level1
         leftSum[0] = nums[0];
         rightSum[len - 1] = nums[len - 1];
 
-        for(int i=1; i < len; i++)
+        for (int i = 1; i < len; i++)
         {
             leftSum[i] = nums[i] + leftSum[i - 1];
             rightSum[len - 1 - i] = nums[len - 1 - i] + rightSum[len - i];
         }
 
-        for(int i = 0; i < len; i++)
+        for (int i = 0; i < len; i++)
         {
             if (leftSum[i] == rightSum[i])
                 return i;
-        }        
+        }
         return -1;
+    }
+
+    public static bool IsIsomorphic(string s, string t)
+    {
+        if (s.Length != t.Length)
+            return false;
+        Dictionary<char, char> tracker = new Dictionary<char, char>();
+        
+        for (int i = 0; i < s.Length; i++)
+        {
+            if (tracker.ContainsKey(s[i])){
+                if (tracker[s[i]] != t[i])
+                    return false;
+            }
+            else
+            {
+                if (tracker.ContainsValue(t[i]))
+                    return false;
+                tracker.Add(s[i], t[i]);
+            }
+        }
+        
+        return true;
+    }
+
+    public static bool IsSubsequence(string s, string t)
+    {
+        if(s.Length > t.Length || s == null || t == null)
+        {
+            return false;
+        }
+
+        int searchIndex = 0;
+        HashSet<int> tracker = new HashSet<int>();
+        for (int i = 0; i < s.Length; i++)
+        {
+            if (tracker.Contains(t.IndexOf(s[i])))
+            {
+                return false;
+            }
+            if (!t.Contains(s[i]))
+                return false;
+
+            else
+            {
+                if (searchIndex > t.IndexOf(s[i])) 
+                    return false;
+                searchIndex = t.IndexOf(s[i]);
+                tracker.Add(searchIndex);
+            }
+
+        }
+        return true;
     }
 }
 
--- CodingChallenges/Program.cs
+++ CodingChallenges/Program.cs
@@ -5,6 +5,14 @@ Console.WriteLine("1480. Running Sum of 1d Array");
 Console.WriteLine("*****************************");
 Console.WriteLine(Level1.RunningSum(new int[] { 1, 5, 2, 6, 7, 4}));
 
-Console.WriteLine("724.Find Pivot Index");
+Console.WriteLine("\n724.Find Pivot Index");
 Console.WriteLine("********************");
 Console.WriteLine(Level1.PivotIndex(new int[] { 1, 7, 3, 6, 5, 6 }));
+
+Console.WriteLine("\n205.Isomorphic Strings");
+Console.WriteLine("**********************");
+Console.WriteLine(Level1.IsIsomorphic("bbbaaaba", "aaabbbba"));
+
+Console.WriteLine("\n392. Is Subsequence");
+Console.WriteLine("**********************");
+Console.WriteLine(Level1.IsSubsequence("ab", "baab"));
