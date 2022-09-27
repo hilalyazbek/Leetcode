69d02991d03481dff80fc0abf4c364cb0eec58a7
Hilal Yazbek <hilal_yazbeck@hotmail.com>
2022-09-27 18:18:52 +0300

    //35. Search Insert Position

--- CodingChallenges/AlgorithmI.cs
+++ CodingChallenges/AlgorithmI.cs
@@ -80,4 +80,30 @@ internal class AlgorithmI
 
         return low;
     }
+
+    //35. Search Insert Position
+    public int SearchInsert(int[] nums, int target)
+    {
+        if (nums == null || nums.Length == 0)
+            return -1;
+
+        int i = 0,
+            j = nums.Length - 1;
+
+        while (i <= j)
+        {
+            int m = j + (i - j) / 2;
+
+            if (nums[m] == target)
+                return m;
+            else if (nums[m] < target)
+                i = m + 1;
+            else
+                j = m - 1;
+        }
+
+        return i;
+    }
+
+
 }
