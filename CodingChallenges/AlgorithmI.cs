eb938039d9b6b27a27bbb14c11859f210080e386
Hilal Yazbek <hilal_yazbeck@hotmail.com>
2022-09-27 23:11:06 +0300

    70. Climbing Stairs

--- CodingChallenges/AlgorithmI.cs
+++ CodingChallenges/AlgorithmI.cs
@@ -116,4 +116,20 @@ internal class AlgorithmI
         return Fib(n - 1) + Fib(n - 2);
     }
 
+    public int ClimbStairs(int n)
+    {
+        if (n == 1) return 1;
+        if (n == 2) return 2;
+        int oneStep = 1;
+        int twoStep = 2;
+
+        for (int i = 3; i <= n; i++)
+        {
+            int temp = twoStep;
+            twoStep = oneStep + twoStep;
+            oneStep = temp;
+        }
+        return twoStep;
+    }
+
 }
