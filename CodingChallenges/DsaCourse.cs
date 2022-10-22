802ba69d31115a6a3ab359333889c5c534f8e73f
Hilal Yazbek <hilal_yazbeck@hotmail.com>
2022-10-22 13:27:28 +0300

    union find with path compression

--- CodingChallenges/DsaCourse.cs
+++ CodingChallenges/DsaCourse.cs
@@ -1,16 +1,20 @@
 public class DSACourse_UnionFind
 {
     private int[] ids { get; set; }
+    private int[] size { get; set; }
 
     // O(n)
     public DSACourse_UnionFind(int n)
     {
+        size = new int[n];
         ids = new int[n];
         for (int i = 0; i < n; i++)
         {
             ids[i] = i;
         }
     }
+
+    #region Quick Find - SLOW
 
     //quick find algorithm O(1)
     public bool IsConnected(int p, int q)
@@ -35,7 +39,9 @@ public class DSACourse_UnionFind
             }
         }
     }
+    #endregion
 
+    #region Quick Union - Still slow but better than quick find.
     public void QuickUnion(int p, int q)
     {
         int i = GetRoot(p);
@@ -58,4 +64,39 @@ public class DSACourse_UnionFind
     {
         return GetRoot(p) == GetRoot(q);
     }
+
+    #endregion
+
+    #region Weighted Quick Union
+    public void WeightedQuickUnion(int p, int q)
+    {
+        int i = GetRoot(p);
+        int j = GetRoot(q);
+        if (i == j) { return; }
+
+        if (size[i] < size[j])
+        {
+            ids[i] = j;
+            size[j] += size[i];
+        }
+        else
+        {
+            ids[j] = i;
+            size[i] += size[j];
+        }
+    }
+    #endregion
+
+    #region Path Compression
+    // make every node point to its grandparent node, to make the tree as flat as possible
+    public int GetRootPathCompression(int i)
+    {
+        while (i != ids[i])
+        {
+            ids[i] = ids[ids[i]]; // set id of each node to the root.
+            i = ids[i];
+        }
+        return i;
+    }
+    #endregion
 }
