a942f0f912ac5061d9d08cd870fd02591d9008bd
Hilal Yazbek <hilal_yazbeck@hotmail.com>
2022-11-04 23:56:05 +0300

    imlemented DFS on graph

--- CodingChallenges/Graph.cs
+++ CodingChallenges/Graph.cs
@@ -23,3 +23,44 @@ public class Graph
         return Adj[v];
     }
 }
+
+public class Paths
+{
+    public Graph? G { get; set; }
+    public int Source { get; set; }
+
+    public Paths(Graph g, int s)
+    {
+        this.G = G;
+        this.Source = s;
+    }
+}
+
+public class DepthFirstPaths
+{
+    public bool[]? Visited { get; set; }
+    public int[]? EdgeTo { get; set; }
+    public int S { get; set; }
+    public DepthFirstPaths(Graph G, int s)
+    {
+        G = new Graph(4);
+        Visited = new bool[4];
+        EdgeTo = new int[4];
+        this.S = s;
+
+        DFS(G, S);
+    }
+
+    private void DFS(Graph g, int vertex)
+    {
+        Visited[vertex] = true;
+        foreach (int w in g.AdjVertices(vertex))
+        {
+            if (!Visited[w])
+            {
+                DFS(g, w);
+                EdgeTo[w] = vertex;
+            }
+        }
+    }
+}
