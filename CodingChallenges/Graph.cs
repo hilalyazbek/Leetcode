5b3b6820591c31d5d933edc1fb3664dfa9ab8fb1
Hilal Yazbek <hilal_yazbeck@hotmail.com>
2022-11-04 15:58:45 +0300

    initial graph implementation

--- CodingChallenges/Graph.cs
+++ CodingChallenges/Graph.cs
@@ -1,4 +1,25 @@
-public static class Graph
+public class Graph
 {
-
+    public int Vertices { get; set; }
+    public List<int>[] Adj { get; set; }
+    public Graph(int V)
+    {
+        //create list<int> for each vertex.
+        Vertices = V;
+        Adj = new List<int>[V];
+        for (int i = 0; i < V; i++)
+        {
+            Adj[i] = new List<int>();
+        }
+    }
+    //add a link between 2 vertices (v and w)
+    public void AddEdge(int v, int w)
+    {
+        Adj[v].Add(w);
+        Adj[w].Add(v);
+    }
+    public List<int> AdjVertices(int v)
+    {
+        return Adj[v];
+    }
 }
