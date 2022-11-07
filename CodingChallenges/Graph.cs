78d926d02ca228647137a03cd188646daa21b3c9
Hilal Yazbek <hilal_yazbeck@hotmail.com>
2022-11-07 18:00:36 +0300

    simpler DFS method

--- CodingChallenges/Graph.cs
+++ CodingChallenges/Graph.cs
@@ -22,63 +22,26 @@ public class Graph
     {
         return Adj[v];
     }
-}
 
-public class Paths
-{
-    public Graph? G { get; set; }
-    public int Source { get; set; }
-
-    public Paths(Graph g, int s)
+    //DFS traversal starting from "vertex" 
+    public void DFS(int vertex)
     {
-        this.G = G;
-        this.Source = s;
-    }
-}
-
-public class DepthFirstPaths
-{
-    public bool[]? Visited { get; set; }
-    public int[]? EdgeTo { get; set; }
-    public int S { get; set; }
-    public DepthFirstPaths(Graph G, int s)
-    {
-        G = new Graph(4);
-        Visited = new bool[4];
-        EdgeTo = new int[4];
-        this.S = s;
-
-        DFS(G, S);
-    }
-    public bool HasPathTo(int vertex)
-    {
-        return Visited[vertex];
+        bool[] visited = new bool[vertex];
+        DFSUtil(vertex, visited);
     }
 
-    public Stack<int> PathTo(int vertex)
+    private void DFSUtil(int v, bool[] visited)
     {
-        if (!HasPathTo(vertex)) return null;
-        Stack<int> path = new();
-        for (int i = vertex; i != S; i = EdgeTo[i])
+        //mark current node as visited, and print it
+        visited[v] = true;
+        Console.WriteLine(v);
+
+        //recurse all the adj vertices of v
+        List<int> adjacent = AdjVertices(v);
+        foreach (int i in adjacent)
         {
-            path.Push(i);
-        }
-        path.Push(S);
-
-        return path;
-
-    }
-    private void DFS(Graph g, int vertex)
-    {
-        Visited[vertex] = true;
-        foreach (int w in g.AdjVertices(vertex))
-        {
-            if (!Visited[w])
-            {
-                DFS(g, w);
-                EdgeTo[w] = vertex;
-            }
+            if (!visited[i])
+                DFSUtil(i, visited);
         }
     }
-
 }
