6bb1cc0434870e40df76fe21ebb5d971d7d310bc
Hilal Yazbek <hilal_yazbeck@hotmail.com>
2022-11-08 18:59:04 +0300

    connected components

--- CodingChallenges/Graph.cs
+++ CodingChallenges/Graph.cs
@@ -2,6 +2,8 @@ public class Graph
 {
     public int Vertices { get; set; }
     public List<int>[] Adj { get; set; }
+    public int[] CC { get; set; }
+    public int Count { get; set; }
     public Graph(int V)
     {
         //create list<int> for each vertex.
@@ -11,6 +13,9 @@ public class Graph
         {
             Adj[i] = new List<int>();
         }
+
+        CC = new int[V]; // array used to keep track of each vertex and its group
+        Count = 0; // used as component group id
     }
     //add a link between 2 vertices (v and w)
     public void AddEdge(int v, int w)
@@ -35,6 +40,9 @@ public class Graph
         //mark current node as visited, and print it
         visited[v] = true;
         Console.WriteLine(v);
+
+        //assign same group id for every connected component
+        CC[v] = Count;
 
         //recurse all the adj vertices of v
         List<int> adjacent = AdjVertices(v);
@@ -64,5 +72,11 @@ public class Graph
                 queue.Enqueue(i);
             }
         }
+    }
+
+    public void ConnectedComponents(int vertex)
+    {
+        DFS(vertex);
+        Count++;
     }
 }
