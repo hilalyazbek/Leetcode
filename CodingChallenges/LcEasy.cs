e59fb15ede3ba8e082df8b3b6e8b5628dfa8bd2d
Hilal Yazbek <hilal_yazbeck@hotmail.com>
2022-12-07 23:32:21 +0300

    11. Container With Most Water

--- CodingChallenges/LcEasy.cs
+++ CodingChallenges/LcEasy.cs
@@ -504,4 +504,27 @@ public static class LcEasy
 
         return sum;
     }
+
+    //11. Container With Most Water
+    public static int MaxArea(int[] height)
+    {
+        int left = 0;
+        int right = height.Length - 1;
+        int max = 0;
+
+        while (left < right)
+        {
+            max = Math.Max(Math.Min(height[left], height[right]) * (right - left), max);
+
+            if (height[left] > height[right])
+            {
+                right--;
+            }
+            else
+            {
+                left++;
+            }
+        }
+        return max;
+    }
 }
